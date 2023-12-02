d07de8ba755b3b85118b0a14d021c2783924d594
Vitalii <35968866+vkrivulya@users.noreply.github.com>
2023-12-02 16:13:17 +0200

    Add second Homework (#2)
    * HW-02: Add second Homework
    
    * HW-02: Add second Homework
    
    * HW-02: Add second Homework
    
    ---------
    
    Co-authored-by: v.kryvulia <vitaliy.kryvulia@corp.kismia.com>

---
+++ src/main/java/pages/secondHomework/CurrencyConverter.java
@@ -0,0 +1,24 @@
+package pages.secondHomework;
+
+public class CurrencyConverter {
+
+    private final double commission;
+    private final double exchangeRate;
+
+    public CurrencyConverter(final double commission, final double exchangeRate) {
+        this.commission = commission;
+        this.exchangeRate = exchangeRate;
+    }
+
+    public double convertUsdToUah(final double amount) {
+        return amount * exchangeRate;
+    }
+
+    public double calculateCommission(final double amount) {
+        return (commission / 100) * amount;
+    }
+
+    public double calculateTotalAmount(final double amountUah, final double commissionInUah) {
+        return amountUah - commissionInUah;
+    }
+}
